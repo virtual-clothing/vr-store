6a4a27c00f95fce5c2a295c59ec7dfcdeaaded1f
DalThePal <dallen.hoyal@gmail.com>
2018-05-08 17:33:01 -0600

    working on popup

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import Nav from './components/navbar.js';
 import Footer from './components/Footer';
 import Routes from './routes.js'
+import Popup from 'react-popup';
 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Popup/>
         <Nav />
         <Routes />
         <Footer />
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
+import Popup from 'react-popup';
 
 export default class Footer extends Component {
 
+    state = {
+        name: '',
+        subject: '',
+        message: ''
+    }
+
+    sendMessage(obj) {
+
+    }
 
     render() {
 
@@ -28,10 +38,32 @@ export default class Footer extends Component {
         const StyledATag = styled.a`
             margin: 10px;
         `
-
+        const Content = () => {
+            return (
+                <div>
+                    <input placeholder='name'/>
+                    <input placeholder='subject'/>
+                    <input/>
+                </div>
+            )
+        }
         return (
             <StyledFooter>
-                <button onClick={() => alert('contact us')}>CONTACT US</button>
+                <button onClick={() => Popup.create({
+                    title: 'CONTACT US',
+                    content:  <Content/>,
+                    buttons: {
+                        left: [{
+                            text: 'Cancel',
+                            action: () => {Popup.close()}
+                        }],
+
+                        right: [{
+                            text: 'Send',
+                            action: this.sendMessage({})
+                        }]
+                    }
+                })}>CONTACT US</button>
                 <SocialMediaLinks>
                     <StyledATag><img src='./twitter.png' alt='twitter' height='50%' width='50%'/></StyledATag>
                     <StyledATag><img src='./facebook.png' alt='facebook' height='50%' width='50%'/></StyledATag>
