4fe530cf848e2723d32862a0f955c6f2a9f5cc0e
Munkhtegsh Munkhbat <munkhtegsh.m@gmail.com>
2018-05-09 14:29:38 -0600

    Add user's gender, nickname, email to the db

--- db/createUser.sql
+++ db/createUser.sql
@@ -1,4 +1,4 @@
-insert into users (username, auth_id, profile_img)
-values ($1, $2, $3)
+insert into users (username, auth_id, profile_img, gender, nickname, email)
+values ($1, $2, $3, $4, $5, $6)
 
 returning *;
