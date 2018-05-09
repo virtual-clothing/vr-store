d905dedd0ca82a1b991622c29bba39d787f92714
Munkhtegsh Munkhbat <munkhtegsh.m@gmail.com>
2018-05-09 11:37:45 -0600

    Redux works and userInfo will be downloaded when user logged in

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,7 +1,32 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {getUserInfo} from '../ducks/reducer';
 
-export default () => (
-  <div>
-    Success! User authenticated here... Please pull the all user's data in this page!
-  </div>
-)
+class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      userInfo: {}
+    }
+  }
+
+  componentDidMount() {
+    this.props.getUserInfo() 
+  }
+
+  render() {
+    return (
+      <div>
+        Success! User authenticated here... Please pull the all user's data in this page!
+      </div>
+    )
+  }
+  
+}
+
+const maptStateToProps = (state) => {
+  return {
+    userInfo: state.userInfo
+  }
+}
+export default connect(maptStateToProps, {getUserInfo})(Login)
