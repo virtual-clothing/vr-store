a6c188b6fb39e38b206eaac61a150e5486df062d
jesseh42t <jessehartwilliams@gmail.com>
2018-05-10 10:22:24 -0600

    stripe is working

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
-
+import Checkout from './stripe/Checkout'
 export default class Cart extends Component {
 
     render() {
         return (
             <div>
+                <Checkout/>
                 CART
             </div>
         )
