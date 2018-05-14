f3d57efb96416873d31fac5d3411ca788b9d38e2
DalThePal <dallen.hoyal@gmail.com>
2018-05-14 15:02:13 -0600

    remove from cart

--- db/getUserCart.sql
+++ db/getUserCart.sql
@@ -1,2 +1,5 @@
-select * from cart
-where user_id = $1
+
+SELECT cart.user_id, products.title, products.price, products.product_img, product_id
+FROM cart
+FULL OUTER JOIN products ON cart.product_id=products.id
+WHERE user_id = $1;
--- db/remFromCart.sql
+++ db/remFromCart.sql
@@ -1,4 +1,6 @@
-delete from table cart
-where user_id = $1 and name = $2;
-select * from cart
-where user_id = $1;
+delete from cart
+where user_id = $1 and product_id = $2;
+SELECT cart.user_id, products.title, products.price, products.product_img, product_id
+FROM cart
+FULL OUTER JOIN products ON cart.product_id=products.id
+WHERE user_id = $1;
