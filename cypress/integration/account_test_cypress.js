ebfbeee27894ded12446d94c5feae560899b80e0
Eric Ross <ericgarth99@gmail.com>
2018-05-23 17:33:34 -0600

    about to merge

--- cypress/integration/account_test_cypress.js
+++ cypress/integration/account_test_cypress.js
@@ -1,6 +1,6 @@
 describe('testing Home page', () =>{
     it('navigate to item from home page', () => {
-        cy.visit('localhost:3000')
+        cy.visit('localhost:4444')
         cy.get('a')
             .contains('Men')
             .should('contain', 'Men')
@@ -10,6 +10,11 @@ describe('testing Home page', () =>{
         cy.get('div.sc-eqIVtm.gZZquL')
             .contains('m')
             .click();
-
     })
+    // it('other stuff', () => {
+    //     cy.visit('localhost:4444')
+    //     cy.get('a')
+    //         .contains('Women')
+    //         .should('contain', 'Woman')
+    // })
 })
